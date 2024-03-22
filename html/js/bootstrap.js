96ed2666520e296d4a89308222d8daa46e1403cc
tanushree050203 <142522884+tanushree050203@users.noreply.github.com>
2024-03-22 04:06:15 -0700

    bootstrap.js

--- html/js/bootstrap.js
+++ html/js/bootstrap.js
@@ -1,28 +1,28 @@
-  /**
-   * ------------------------------------------------------------------------
-   * Constants
-   * ------------------------------------------------------------------------
-   */
+/**
+ * ------------------------------------------------------------------------
+ * Constants
+ * ------------------------------------------------------------------------
+ */
 
-  var NAME$1 = 'button';
-  var VERSION$1 = '4.3.1';
-  var DATA_KEY$1 = 'bs.button';
-  var EVENT_KEY$1 = "." + DATA_KEY$1;
-  var DATA_API_KEY$1 = '.data-api';
-  var JQUERY_NO_CONFLICT$1 = $.fn[NAME$1];
-  var ClassName$1 = {
+var NAME$1 = 'button';
+var VERSION$1 = '4.3.1';
+var DATA_KEY$1 = 'bs.button';
+var EVENT_KEY$1 = "." + DATA_KEY$1;
+var DATA_API_KEY$1 = '.data-api';
+var JQUERY_NO_CONFLICT$1 = $.fn[NAME$1];
+var ClassName$1 = {
     ACTIVE: 'active',
     BUTTON: 'btn',
     FOCUS: 'focus'
-  };
-  var Selector$1 = {
+};
+var Selector$1 = {
     DATA_TOGGLE_CARROT: '[data-toggle^="button"]',
     DATA_TOGGLE: '[data-toggle="buttons"]',
     INPUT: 'input:not([type="hidden"])',
     ACTIVE: '.active',
     BUTTON: '.btn'
-  };
-  var Event$1 = {
+};
+var Event$1 = {
     CLICK_DATA_API: "click" + EVENT_KEY$1 + DATA_API_KEY$1,
     FOCUS_BLUR_DATA_API: "focus" + EVENT_KEY$1 + DATA_API_KEY$1 + " " + ("blur" + EVENT_KEY$1 + DATA_API_KEY$1)
     /**
@@ -31,14 +31,90 @@
      * ------------------------------------------------------------------------
      */
 
-  };
+};
 
-  var Button =
+var Button =
     /*#__PURE__*/
     function () {
-      function Button(element) {
-        this._element = element;
-      } // Getters
+        function Button(element) {
+            this._element = element;
+        } // Getters
 
 
-    }
+        var _proto = Button.prototype;
+
+        // Public
+        _proto.toggle = function toggle() {
+            var triggerChangeEvent = true;
+            var addAriaPressed = true;
+            var rootElement = $(this._element).closest(Selector$1.DATA_TOGGLE)[0];
+
+            if (rootElement) {
+                var input = this._element.querySelector(Selector$1.INPUT);
+
+                if (input) {
+                    if (input.type === 'radio') {
+                        if (input.checked && this._element.classList.contains(ClassName$1.ACTIVE)) {
+                            triggerChangeEvent = false;
+                        } else {
+                            var activeElement = rootElement.querySelector(Selector$1.ACTIVE);
+
+                            if (activeElement) {
+                                $(activeElement).removeClass(ClassName$1.ACTIVE);
+                            }
+                        }
+                    }
+
+                    if (triggerChangeEvent) {
+                        if (input.hasAttribute('disabled') || rootElement.hasAttribute('disabled') || input.classList.contains('disabled') || rootElement.classList.contains('disabled')) {
+                            return;
+                        }
+
+                        input.checked = !this._element.classList.contains(ClassName$1.ACTIVE);
+                        $(input).trigger('change');
+                    }
+
+                    input.focus();
+                    addAriaPressed = false;
+                }
+            }
+
+            if (addAriaPressed) {
+                this._element.setAttribute('aria-pressed', !this._element.classList.contains(ClassName$1.ACTIVE));
+            }
+
+            if (triggerChangeEvent) {
+                $(this._element).toggleClass(ClassName$1.ACTIVE);
+            }
+        };
+
+        _proto.dispose = function dispose() {
+            $.removeData(this._element, DATA_KEY$1);
+            this._element = null;
+        } // Static
+            ;
+
+        Button._jQueryInterface = function _jQueryInterface(config) {
+            return this.each(function () {
+                var data = $(this).data(DATA_KEY$1);
+
+                if (!data) {
+                    data = new Button(this);
+                    $(this).data(DATA_KEY$1, data);
+                }
+
+                if (config === 'toggle') {
+                    data[config]();
+                }
+            });
+        };
+
+        _createClass(Button, null, [{
+            key: "VERSION",
+            get: function get() {
+                return VERSION$1;
+            }
+        }]);
+
+        return Button;
+    }();
